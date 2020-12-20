eb7e22f7502c5fae7d0e88fe3828388c4689d1ea
sarahCamargo <sarahcamargo00@gmail.com>
2020-12-19 21:30:56 -0300

    Creating Position class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Position position = new Position(2, 4);
+		System.out.println(position);
 
 	}
 
